946f040afc28df89bdee9fe625f9d3e015c778f6
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-05-19 03:02:51 +0600

    task done btn added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 // middle wares ------------------- 
 app.use(cors())
@@ -32,7 +32,32 @@ async function run() {
             const result = await toDoCollections.find(query).toArray()
             res.send(result)
         })
+        // delete a task using _id 
 
+        app.delete('/task/:id', async (req, res) => {
+            console.log('hello')
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await toDoCollections.deleteOne(query)
+            res.send(result)
+        })
+        // update data by id using put
+
+        app.put('/task/:id', async (req, res) => {
+            console.log('hello')
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            // create a document that sets the plot of the movie
+            const updateDoc = {
+                $set: {
+                    status: 'done'
+                },
+            };
+
+            const result = await toDoCollections.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
 
     } finally {
 
