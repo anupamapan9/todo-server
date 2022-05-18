733fd4aacc254a9fd90d7c54b2abc17e3782eb55
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-05-19 01:07:48 +0600

    post aip added

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+// middle wares ------------------- 
+app.use(cors())
+app.use(express.json())
+
+const uri = "mongodb+srv://toDoAdmin:QmqoFXRuJY75wOo1@cluster0.f4xqs.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+        await client.connect();
+        const toDoCollections = client.db("toDoApp").collection("toDoTask");
+
+        // insert a task 
+        app.post('/task', async (req, res) => {
+            const toDo = req.body;
+            const result = await toDoCollections.insertOne(toDo)
+            res.send(result)
+        })
+
+
+    } finally {
+
+    }
+
+}
+
+
+run().catch(console.dir)
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('inside World!')
 })
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
